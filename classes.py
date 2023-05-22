21bd574457ed9316b06e3459e3b3663a9ef8fd29
LuccaMS <saintangel961@gmail.com>
2023-05-22 10:40:30 -0300

    teste inicial

--- classes.py
+++ classes.py
@@ -1,24 +1,25 @@
 class Communication():
-    def __init__(self, qtd_emissor=3, qtd_recebedor=3):
+    def __init__(self):
         #Creating the emissor and recebedor lists
-        self.emissor = []
-        self.recebedor = []
+        self.emissores = []
+        self.recebedores = []
+        self.list_of_who_has_the_token = []
+    
+    def rotate_token(self):
+        #once we call this function, we have to circle the token around the emissors to give each one the chance to send a message
+        
+        #we are going to use a list of the size of the emissor list and the only one that has 1 is the one that has the token
+        #respecting the index of the emissor list
+        #first we have to verify if all the list is 0, if it is, then, we have to give the token to the first emissor
+        if self.list_of_who_has_the_token == [0 for i in range(len(self.emissores))]:
+            self.list_of_who_has_the_token[0] = 1
+        else:
+            current_index = self.list_of_who_has_the_token.index(1)
+            next_index = (current_index + 1) % len(self.list_of_who_has_the_token)
+            self.list_of_who_has_the_token[current_index] = 0
+            self.list_of_who_has_the_token[next_index] = 1
 
-        #Creating the emissor and recebedor objects
-
-        for i in range(qtd_recebedor):
-            self.recebedor.append(Recebedor(i))
-
-        for i in range(qtd_emissor):
-            self.emissor.append(Emissor(i,token=None,list_recebedores=self.recebedor))
-
-    def teste(self):
-        self.emissor[0].token = True
-        self.emissor[0].send_message("Hello World",self.emissor[0].token,2)
-
-        self.emissor[1].send_message("Hello World",self.emissor[1].token,2)
-
-class Emissor(Communication):
+class Emissor():
     def __init__(self,id,token=None,list_recebedores=[]):
         self.id = id
         self.token = token
@@ -34,6 +35,7 @@ class Emissor(Communication):
         #Agora precisamos verificar se o emissor tem direito de enviar a mensagem para o recebedor
         #Para isso, precisamos verificar se o token é nulo ou não
         if token == None:
+            print("token is none")
             pass
         else:
             #Se o token não for nulo, então, o emissor tem direito de enviar a mensagem para o recebedor
@@ -48,7 +50,7 @@ class Emissor(Communication):
                 #para o próximo recebedor
                 return False
 
-class Recebedor(Communication):
+class Recebedor():
     def __init__(self,id):
         self.id = id
         pass
